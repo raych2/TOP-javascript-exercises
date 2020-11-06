e1c11306a81b786e0375dd388c2877d63e6c4e23
raych2 <rtongco@gmail.com>
2020-11-05 18:49:24 -0800

    Completed fibonacci, getTheTitles, and findTheOldest

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return 'OOPS';
+    }
+    let arr = [1,1];
+    for (let i = 2; i < num + 1; i++) {
+        arr.push(arr[i - 1] + arr[i - 2]);
+    }
+    return arr[num - 1];
 }
 
 module.exports = fibonacci
--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-let findTheOldest = function() {
+let findTheOldest = function(arr) {
+    return arr.reduce((oldestPerson, currentPerson) => {
+        let oldestAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+        let currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge > currentAge ? oldestPerson : currentPerson;
+    })
+}
 
+function getAge (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 }
 
 module.exports = findTheOldest
